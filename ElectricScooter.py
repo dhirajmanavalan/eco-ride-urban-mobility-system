4d6fbe5823df9af5e372247f56d760893368f098
Dhirajkumar <kumardhiraj7720@gmail.com>
2026-01-07 17:14:23 +0530

    UC3: Add ElectricCar and ElectricScooter using inheritance

---
+++ ElectricScooter.py
@@ -0,0 +1,8 @@
+from Vehicle import Vehicle
+
+class ElectricScooter(Vehicle):
+    def __init__(self, vehicle_id, model, battery_percentage, maintenance_status, rental_price, max_speed_limit):
+        super().__init__(vehicle_id, model, battery_percentage, maintenance_status, rental_price)
+        self.max_speed_limit = max_speed_limit
+    
+    
